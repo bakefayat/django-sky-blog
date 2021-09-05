9265d9059e48acaa53a56569c253c7ef11819638
ehsan <bakefayat98@gmail.com>
2021-09-05 12:26:12 +0430

    article pages and user pages 404 not found problem fixed

--- webdev/web/urls.py
+++ webdev/web/urls.py
@@ -4,10 +4,10 @@ from .views import ArticleList, ArticleDetail, CategoryList, UserList
 app_name = 'blog'
 urlpatterns = [
     path('', ArticleList.as_view(), name='index'),
-    path('page/<int:page>', ArticleList.as_view(), name='index'),
-    path('<str:slug>', ArticleDetail.as_view(), name='single'),
-    path('category/<str:slug>', CategoryList.as_view(), name='category'),
-    path('category/<str:slug>/page/<int:page>', CategoryList.as_view(), name='category'),
-    path('user/<str:slug>', UserList.as_view(), name='user'),
-    path('user/<str:slug>/page/<int:page>', UserList.as_view(), name='user'),
+    path('page/<int:page>/', ArticleList.as_view(), name='index'),
+    path('<str:slug>/', ArticleDetail.as_view(), name='single'),
+    path('category/<str:slug>/', CategoryList.as_view(), name='category'),
+    path('category/<str:slug>/page/<int:page>/', CategoryList.as_view(), name='category'),
+    path('user/<str:slug>/', UserList.as_view(), name='user'),
+    path('user/<str:slug>/page/<int:page>/', UserList.as_view(), name='user'),
 ]
--- webdev/webdev/urls.py
+++ webdev/webdev/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('', include('web.urls')),
     path('admin/', admin.site.urls),
+    path('', include('web.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
