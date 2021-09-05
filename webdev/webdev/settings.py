a04fd19fed9fe0ce604cf7d9c6f31b2b25f97d23
ehsan <bakefayat98@gmail.com>
2021-09-05 15:47:04 +0430

    login page created

--- webdev/webdev/settings.py
+++ webdev/webdev/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = 'account:home'
+
+LOGIN_URL = 'account:login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'web.apps.WebConfig',
     'api',
+    'account.apps.AccountConfig',
     'extentions',
 ]
 
@@ -58,7 +62,7 @@ ROOT_URLCONF = 'webdev.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- webdev/webdev/urls.py
+++ webdev/webdev/urls.py
@@ -19,5 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls'), name='home'),
     path('', include('web.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
