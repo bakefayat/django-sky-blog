57e4bd264f0bcf5c95345927fd11991548f48ea1
ehsan <bakefayat98@gmail.com>
2021-09-06 16:31:07 +0430

    login page makes beautiful

--- webdev/account/views.py
+++ webdev/account/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import ListView
+from web.models import Blog
 
 # Create your views here.
 
 @login_required
 def home(request):
     return render(request, 'registration/home.html')
+
+
+class home_blog(LoginRequiredMixin, ListView):
+    queryset = Blog.objects.published()
+    paginate_by = 2
+    template_name = 'blog/articleList.html'
