2797da1cf9040d0373f6d1812b4a7ecd149cf43d
ehsan <bakefayat98@gmail.com>
2021-09-14 18:30:13 +0430

    Customized fields added to our User model

--- webdev/account/models.py
+++ webdev/account/models.py
@@ -1,4 +1,12 @@
+from django.utils import timezone
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    is_author = models.BooleanField(default=False, verbose_name='نویسنده')
+    special_user = models.DateTimeField(default=timezone.now, verbose_name='ویژه تا', )
+
+    def is_specialuser(self):
+        if self.special_user > timezone.now():
+            return True
+        return False
