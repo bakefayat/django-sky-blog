eca465e6f583ec369414a1f4a4bfb46a1a2bd740
ehsan <bakefayat98@gmail.com>
2021-09-26 09:22:01 +0330

    active Templatetag added

--- webdev/web/templatetags/tags.py
+++ webdev/web/templatetags/tags.py
@@ -6,8 +6,18 @@ register = template.Library()
 def title():
     return 'ورزش 4'
 
-
 @register.inclusion_tag('blog/navbar.html')
 def navbar_cat():
     cat = {'categories': Category.objects.all().filter(display=True)}
     return cat
+
+@register.inclusion_tag('registration/active.html')
+def active(request, content, url_name, fa ):
+    context = {
+        'request': request,
+        'content': content,
+        'url_name': url_name,
+        'url': f'account:{url_name}',
+        'fa': fa,
+    }
+    return context
