5d301058c7fd922343fbbf8d807c65cca0e92f8c
bakefayat <bakefayat98@gmail.com>
2021-12-26 12:40:46 +0330

    search form now only shows published articles

--- core/mixins.py
+++ core/mixins.py
@@ -1,8 +1,9 @@
 class SearchActionMixin:
     def get_queryset(self):
         queryset = super().get_queryset()
+        # only show published ones.
+        queryset = queryset.filter(status="p")
         q = self.request.GET.get('q')
         if q:
-            print('hi')
-            queryset = queryset.filter(title__icontains=q, status="p")
+            queryset = queryset.filter(title__icontains=q)
         return queryset
