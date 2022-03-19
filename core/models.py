7a7729d7003c4438c951fcbffa2c021c78a33bb8
bakefayat <bakefayat98@gmail.com>
2022-03-19 16:52:37 +0330

    SiteProfile added to project to avoid hard coding site data in frontend.

--- core/models.py
+++ core/models.py
@@ -1,7 +1,21 @@
+from django.contrib.sites.models import Site
 from django.db import models
 
 
-# Create your models here.
+class SiteProfile(models.Model):
+
+    title = models.CharField(max_length=100, verbose_name="عنوان سایت")
+    description = models.TextField(max_length=200, verbose_name="توضیحات سایت")
+    site = models.OneToOneField(Site, on_delete=models.SET_NULL, null=True, verbose_name="آدرس سایت")
+
+    class Meta:
+        verbose_name = "تنظیم سایت"
+        verbose_name_plural = "تنظیمات سایت"
+
+    def __str__(self):
+        return self.title
+
+
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ساخت")
     modified = models.DateTimeField(auto_now=True, verbose_name="آخرین بروزرسانی")
--- modules/apps.py
+++ modules/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ModulesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'modules'
+    verbose_name = "ابزارک ها"
